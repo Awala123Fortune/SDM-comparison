a8028bf5c628b5eecdcf35a83d0cc12acf1b5db5
Anna Norberg <anna.norberg@helsinki.fi>
2019-04-11 18:27:56 +0300

    removed redundant

--- SCRIPTS/HMSCpipeline.m
+++ SCRIPTS/HMSCpipeline.m
@@ -1,70 +1,22 @@
-
 clc;
 clearvars;
 
-
-job_HMSCbird = batch('jobHMSCbirds');
-job_HMSCbutterfly_dsz3 = batch('jobHMSCbutter_dsz3');
-job_HMSCplant_dsz3 = batch('jobHMSCplant_dsz3');
-job_HMSCtree_dsz3 = batch('jobHMSCtree_dsz3');
-job_HMSCveg_dsz3 = batch('jobHMSCveg_dsz3');
-
-% job_HMSCbird_dsz3 = batch('jobHMSCbirds_dsz3');
-% job_HMSCbutterfly_dsz3 = batch('jobHMSCbutter_dsz3');
-% job_HMSCplant_dsz3 = batch('jobHMSCplant_dsz3');
-% job_HMSCtree_dsz3 = batch('jobHMSCtree_dsz3');
-% job_HMSCveg_dsz3 = batch('jobHMSCveg_dsz3');
-
-%job_ssHMSCveg_dsz3 = batch('job_ssHMSCveg_dsz3');
-%job_ssHMSCveg_dsz3_2 = batch('job_ssHMSCveg_dsz3_2');
-
-%job_ssHMSCbird_dsz3 = batch('job_ssHMSCbirds_dsz3');
-%job_ssHMSCplant_dsz3 = batch('job_ssHMSCplant_dsz3');
-
-%job_ssHMSCbutterfly_dsz3 = batch('job_ssHMSCbutter_dsz3');
-%job_ssHMSCtree_dsz3 = batch('job_ssHMSCtree_dsz3');
-
-%job_ssHMSCbird = batch('job_ssHMSCbirds');
-%job_ssHMSCplant = batch('job_ssHMSCplant');
-
-%job_ssHMSCveg = batch('job_ssHMSCveg');
-
-%job_ssHMSCbutterfly = batch('job_ssHMSCbutter');
-%job_ssHMSCtree = batch('job_ssHMSCtree');
-
-% job_HMSCbird_dsz3 = batch('jobHMSCbirds_dsz3');
-% job_HMSCbutterfly_dsz3 = batch('jobHMSCbutter_dsz3');
-% job_HMSCplant_dsz3 = batch('jobHMSCplant_dsz3');
-% job_HMSCtree_dsz3 = batch('jobHMSCtree_dsz3');
-% job_HMSCveg_dsz3 = batch('jobHMSCveg_dsz3');
-
-
-MCMC2=false;
-commSP=true;
-intXs=false;
+MCMC2 = false;
+commSP = false;
+intXs = false;
 
 % pipeine directory
-%wdpath=fullfile('...','bakeoff','pipeline');
-%wdpath=fullfile('/Users/anorberg/OneDrive - University of Helsinki/','bakeoff','pipeline');
-wdpath=fullfile('D:\HY-data\NORBERG\OneDrive - University of Helsinki\','bakeoff','pipeline');
-fpath=fullfile('F:\FITS');
+wdpath = fullfile('...','bakeoff','pipeline');
 
 % run settings
-bakeoffSettings=fullfile(wdpath,'SCRIPTS','settingsHMSCmatlab.m');
-bakeoff_ssSettings=fullfile(wdpath,'SCRIPTS','settings_ssHMSCmatlab.m');
+bakeoffSettings = fullfile(wdpath,'SCRIPTS','settingsHMSCmatlab.m');
+bakeoff_ssSettings = fullfile(wdpath,'SCRIPTS','settings_ssHMSCmatlab.m');
 run(bakeoffSettings)
 
-dsz=1     % data size (1, 2 or 3)
-s=1       % data set
+dsz = 1     % data size (1 or 3)
+s = 1       % data set (1-5)
 
-        %run(bakeoffSettings)
-        %run(fullfile(wdpath,'MODELS','fit_predict_hmsc.m'))    
-        %run(fullfile(wdpath,'MODELS','predict_hmsc.m'))    
-
-        run(bakeoffSettings)
-        run(fullfile(wdpath,'MODELS','fit_predict_hmsc_ss.m'))
-
-
-
- 
-
+run(bakeoffSettings)
+run(fullfile(wdpath,'MODELS','fit_predict_hmsc.m'))    
+run(bakeoff_ssSettings)
+run(fullfile(wdpath,'MODELS','fit_predict_hmsc_ss.m'))
